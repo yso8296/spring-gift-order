384661c5e150a493a578b9f91f94abdbcb012683
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-07-27 01:48:49 +0900

    chore: 필요없는 클래스 삭제

--- src/main/java/gift/common/exception/DuplicateOptionNameException.java
+++
@@ -1,8 +0,0 @@
-package gift.common.exception;
-
-public class DuplicateOptionNameException extends RuntimeException{
-
-    public DuplicateOptionNameException() {
-        super("중복된 옵션 이름입니다.");
-    }
-}
